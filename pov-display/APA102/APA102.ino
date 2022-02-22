4c0ac60e2d8cdf93acabe603a59169322d7881b9
architolk <msj@xs4all.nl>
2022-02-22 22:18:44 +0100

    Made it work

--- pov-display/APA102/APA102.ino
+++ pov-display/APA102/APA102.ino
@@ -2,33 +2,35 @@
 // Using SPI
 #include <SPI.h>
 
-#define LEDS 5
-#define BRIGHTNESS 15
+#define LEDS 20
+#define BRIGHTNESS 5
 
-setup() {
-  SPI.begin();
+SPIClass ledSPI(VSPI);
+
+void setup() {
+  ledSPI.begin();
 }
 
-loop() {
+void loop() {
   //Not sure if really necessary
   //SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
   //Start frame: 32 bits of zeros
-  SPI.transfer(0x00);
-  SPI.transfer(0x00);
-  SPI.transfer(0x00);
-  SPI.transfer(0x00);
+  ledSPI.transfer(0x00);
+  ledSPI.transfer(0x00);
+  ledSPI.transfer(0x00);
+  ledSPI.transfer(0x00);
   for (uint8_t i = 0; i<LEDS; i++) {
-    SPI.transfer(0xE0 + BRIGHTNESS);
-    SPI.transfer(random(256));
-    SPI.transfer(random(256));
-    SPI.transfer(random(256));
+    ledSPI.transfer(0xE0 + BRIGHTNESS);
+    ledSPI.transfer(random(256));
+    ledSPI.transfer(random(256));
+    ledSPI.transfer(random(256));
   }
   //32 bits of zeros are enough for 64 LEDs, might re
   // End frame: 8+8*(leds >> 4) clock cycles
   for (uint8_t i = 0; i<LEDS; i+=16) {
-    SPI.transfer(0x00); // 8 more clock cycles
+    ledSPI.transfer(0x00); // 8 more clock cycles
   }
   //Not sure if really necessary
   //SPI.endTransaction();
-  delay(1000); // One second delay
+  delay(500); // Half a second delay
 }
