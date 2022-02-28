3c1a5c12d59d0d3bc6af9c4afd0f8ba4bbc1e300
architolk <msj@xs4all.nl>
2022-02-28 11:17:36 +0100

    Made it work

--- pov-display/APA102-Bluetooth/APA102-Bluetooth.ino
+++ pov-display/APA102-Bluetooth/APA102-Bluetooth.ino
@@ -13,20 +13,24 @@ BluetoothSerial SerialBT;
 #define BRIGHTNESS 5
 
 #define BUFLENGTH 180
-byte buffer[BUFLENGTH];
+#define RESOLUTION 120
+byte buffer[RESOLUTION][BUFLENGTH];
 
 SPIClass ledSPI(VSPI);
 
-int startupCount = 0;
+int startupCount = -3;
+byte theta = 0;
+byte rotor = 0;
 boolean doOnce = true;
+boolean imgLoaded = false;
 
 void setup() {
   for (uint8_t i=0; i<BUFLENGTH; i++) {
-    buffer[i]=0;
+    buffer[0][i]=0;
   }
-  buffer[0] = 255;
-  buffer[4] = 255;
-  buffer[8] = 255;
+  buffer[0][0] = 255;
+  buffer[0][4] = 255;
+  buffer[0][8] = 255;
   SerialBT.begin("ESP32test"); //Bluetooth device name
   ledSPI.begin();
 }
@@ -42,27 +46,53 @@ void loop() {
     if (startupCount==-3) {
       if (received=255) {
         startupCount++;
+      } else {
+        theta=0;
       }
     } else if (startupCount==-2) {
       if (received==0) {
         startupCount++;
       } else {
         startupCount=-3;
+        theta=0;
       }
     } else if (startupCount==-1) {
       if (received==128) {
         startupCount++;
+        theta=0;
+        imgLoaded=false; //Image in processing of loading, do not show during loading of new image
+        requestThetaIndex(0);
       } else {
         startupCount=-3;
+        theta=0;
       }
     } else {
       //Start receiving
-      buffer[startupCount]=received;
+      buffer[theta][startupCount]=received;
       startupCount++;
       if (startupCount==BUFLENGTH) {
-        startupCount=-3;
-        updateLEDs();
+        startupCount=0;
+        theta++;
+        if (theta<RESOLUTION) {
+          //Resolution not reached, request next line
+          requestThetaIndex(theta);
+        } else {
+          startupCount=-3;
+          theta=0;
+          imgLoaded=true;
+          updateLEDs();
+        }
       }
+    }
+  } else {
+    if (imgLoaded==true) {
+      //Time do to some rotation stuff!
+      delay(100); //Small delay: should change to the real framerate of the rotor
+      rotor++;
+      if (rotor==RESOLUTION) {
+        rotor=0;
+      }
+      updateLEDs();
     }
   }
 }
@@ -78,9 +108,9 @@ void updateLEDs() {
   for (uint8_t i = 0; i<LEDS; i++) {
     ledSPI.transfer(0xE0 + BRIGHTNESS);
     //Buffer contains colors as RGB, the APA102 expects BGR
-    ledSPI.transfer(buffer[i*3+2]); //Blue
-    ledSPI.transfer(buffer[i*3+1]); //Green
-    ledSPI.transfer(buffer[i*3]);   //Red
+    ledSPI.transfer(buffer[rotor][i*3+2]); //Blue
+    ledSPI.transfer(buffer[rotor][i*3+1]); //Green
+    ledSPI.transfer(buffer[rotor][i*3]);   //Red
   }
   //32 bits of zeros are enough for 64 LEDs, might re
   // End frame: 8+8*(leds >> 4) clock cycles
@@ -89,4 +119,12 @@ void updateLEDs() {
   }
   //Not sure if really necessary
   //SPI.endTransaction();
+}
+
+void requestThetaIndex(byte theta) {
+  //Failsafe: theta should not be langer than RESOLUTION
+  if (theta<RESOLUTION) {
+    SerialBT.write(theta);
+    delay(20);
+  }
 }
