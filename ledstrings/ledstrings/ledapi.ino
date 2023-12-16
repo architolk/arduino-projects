96568a3372a332b0c98ced3e23c6785261030e57
architolk <msj@xs4all.nl>
2023-12-16 10:39:55 +0100

    Added functionality for multiple strips

--- ledstrings/ledstrings/ledapi.ino
+++ ledstrings/ledstrings/ledapi.ino
@@ -9,9 +9,18 @@
 
 // How many leds in your strip?
 #define NUM_LEDS 150
+#define NUM_STRIPS 3
 #define MIN_BRIGHT 30
 
-#define DATA_PIN 6
+//With one strip, between 1s-10s seems right, with three strips, interval should be a 1/3 of these values
+//Max blink interval is in miliseconds
+#define MIN_BLINK_INTERVAL 333
+#define MAX_BLINK_INTERVAL 3333
+
+//PINS per Strip. Mind that this must match NUM_STRIPS and setupLEDs() should reflect this!
+#define DATA_PIN0 5
+#define DATA_PIN1 6
+#define DATA_PIN2 7
 
 const int CHASER_LIGHTS[] PROGMEM = {0,10,40,128,255,255,128,40,10,0};
 
@@ -22,6 +31,7 @@ int offset;
 
 CRGB original;
 int blinkingLedNr;
+int blinkingStripNr;
 boolean blinkActive;
 
 time_t starDelay;
@@ -29,10 +39,14 @@ time_t starBlinkTime;
 time_t waveTime;
 
 // Define the array of leds
-CRGB leds[NUM_LEDS];
+// Every strip has a seperate array
+CRGB leds[NUM_STRIPS][NUM_LEDS];
 
 void setupLEDs() {
-  FastLED.addLeds<WS2813, DATA_PIN, GRB>(leds, NUM_LEDS);//.setCorrection(TypicalLEDStrip);
+  //Initialize all strips - this cannot be done with a for loop, because the pin value must be a constant...
+  FastLED.addLeds<WS2813, DATA_PIN0, GRB>(leds[0], NUM_LEDS);
+  FastLED.addLeds<WS2813, DATA_PIN1, GRB>(leds[1], NUM_LEDS);
+  FastLED.addLeds<WS2813, DATA_PIN2, GRB>(leds[2], NUM_LEDS);
   resetStarDelay(false);
   resetWaveTimer();
   initLEDs();
@@ -116,7 +130,7 @@ void resetStarDelay(boolean _blinkActive) {
   if (blinkActive) {
     starDelay = 100;
   } else {
-    starDelay = random(1000,10000);
+    starDelay = random(MIN_BLINK_INTERVAL,MAX_BLINK_INTERVAL);
   }
   starBlinkTime = millis();
 }
@@ -137,55 +151,66 @@ void setLEDColor(int i, int o, int b) {
   byte red = round(247*wave);
   byte green = round(60*wave);
   byte blue = round(4*wave);
-  leds[(i+o)%NUM_LEDS].setRGB(8+red,2+green,blue);
+  for (int x=0; x<NUM_STRIPS; x++) {
+    leds[x][(i+o)%NUM_LEDS].setRGB(8+red,2+green,blue);
+  }
 }
 
 void initLEDsOff() {
-  for (int i=0; i<NUM_LEDS; i++) {
-    leds[i] = 0;
+  for (int x=0; x<NUM_STRIPS; x++) {
+    for (int i=0; i<NUM_LEDS; i++) {
+      leds[x][i] = 0;
+    }
   }
 }
 
 void initLEDSRedGreen() {
-  int j=0;
-  byte clr = 0;
-  for (int i=0; i<NUM_LEDS; i++) {
-    j++;
-    if (j>10) {
-      j=0;
-      clr = 1-clr;
-    }
-    if (clr==0) {
-      leds[i].setRGB(maxBrightness,0,0);
-    } else {
-      leds[i].setRGB(0,maxBrightness,0);
+  for (int x=0; x<NUM_STRIPS; x++) {
+    int j=0;
+    byte clr = 0;
+    for (int i=0; i<NUM_LEDS; i++) {
+      j++;
+      if (j>10) {
+        j=0;
+        clr = 1-clr;
+      }
+      if (clr==0) {
+        leds[x][i].setRGB(maxBrightness,0,0);
+      } else {
+        leds[x][i].setRGB(0,maxBrightness,0);
+      }
     }
   }
 }
 
 void initLEDSRedWhiteBlue() {
-  for (int i=0; i<NUM_LEDS; i++) {
-    if (i<(NUM_LEDS/3)) {
-      leds[i].setRGB(maxBrightness,0,0);
-    } else if (i<(NUM_LEDS*2/3)) {
-      leds[i].setRGB(maxBrightness,maxBrightness,maxBrightness);
-    } else {
-      leds[i].setRGB(0,0,maxBrightness);
+  for (int x=0; x<NUM_STRIPS; x++) {
+    for (int i=0; i<NUM_LEDS; i++) {
+      if (i<(NUM_LEDS/3)) {
+        leds[x][i].setRGB(maxBrightness,0,0);
+      } else if (i<(NUM_LEDS*2/3)) {
+        leds[x][i].setRGB(maxBrightness,maxBrightness,maxBrightness);
+      } else {
+        leds[x][i].setRGB(0,0,maxBrightness);
+      }
     }
   }
 }
 
 void initRainbow() {
-  for (int i=0; i<NUM_LEDS; i++) {
-    leds[i].setHSV(i*255/(NUM_LEDS-1),255,maxBrightness);
+  for (int x=0; x<NUM_STRIPS; x++) {
+    for (int i=0; i<NUM_LEDS; i++) {
+      leds[x][i].setHSV(i*255/(NUM_LEDS-1),255,maxBrightness);
+    }
   }
 }
 
 void checkDimmedString() {
+  //Only one strip (the first one) will be used for sending a red blinking light
   currentTime = millis();
   if ((currentTime - waveTime) > 1000) {
     for (int i=0; (i<3) && (i<NUM_LEDS); i++) {
-      leds[i] = ((leds[i]==0) ? 0x400000 : 0);
+      leds[0][i] = ((leds[0][i]==0) ? 0x400000 : 0);
     }
     FastLED.show();
     resetWaveTimer();
@@ -195,8 +220,10 @@ void checkDimmedString() {
 void checkChaserString() {
   currentTime = millis();
   if ((currentTime - waveTime) > 10) {
-    for (int i=0; (i<10) && (i<NUM_LEDS); i++) {
-      leds[i+offset].setRGB(CHASER_LIGHTS[i],0,0);
+    for (int x=0; x<NUM_STRIPS; x++) {
+      for (int i=0; (i<10) && (i<NUM_LEDS); i++) {
+        leds[x][i+offset].setRGB(CHASER_LIGHTS[i],0,0);
+      }
     }
     offset = offset + delta;
     if (offset==0) {
@@ -212,9 +239,11 @@ void checkChaserString() {
 }
 
 void setStaircaseStep(int o, int b) {
-  for (int i=1; i<11; i++) {
-    if ((o-i)>0) {
-      leds[o-i].setRGB(b,b,b);
+  for (int x=0; x<NUM_STRIPS; x++) {
+    for (int i=1; i<11; i++) {
+      if ((o-i)>0) {
+        leds[x][o-i].setRGB(b,b,b);
+      }
     }
   }
 }
@@ -264,11 +293,12 @@ void checkLedString() {
    // Reset to original when blink is active, otherwise: set a LED to bright white
    // Blink will be active for 0.1s
    if (blinkActive) {
-     leds[blinkingLedNr] = original;
+     leds[blinkingStripNr][blinkingLedNr] = original;
    } else {
      blinkingLedNr = random(NUM_LEDS);
-     original = leds[blinkingLedNr];
-     leds[blinkingLedNr] = 0xFFFFFF;
+     blinkingStripNr = random(NUM_STRIPS);
+     original = leds[blinkingStripNr][blinkingLedNr];
+     leds[blinkingStripNr][blinkingLedNr] = 0xFFFFFF;
    }
    FastLED.show();
    resetStarDelay(!blinkActive);
